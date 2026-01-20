befd90b5afbaf17059a84d94b3964d29a7b6f091
Tapio Piironen <tapio.piironen@helsinki.fi>
2026-01-20 15:26:07 +0200

    import simple maven archetype project base

---
+++ src/main/java/fi/helsinki/tapio/App.java
@@ -0,0 +1,13 @@
+package fi.helsinki.tapio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
