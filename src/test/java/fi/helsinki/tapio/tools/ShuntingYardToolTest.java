0ad0f48a96a2e72ef34ddcf03fa269d62e02881e
Tapio Piironen <tapio.piironen@helsinki.fi>
2026-01-20 17:12:32 +0200

    add test for ShuntingYardToolTest

---
+++ src/test/java/fi/helsinki/tapio/tools/ShuntingYardToolTest.java
@@ -0,0 +1,12 @@
+package fi.helsinki.tapio.tools;
+
+import junit.framework.TestCase;
+
+public class ShuntingYardToolTest extends TestCase {
+
+    public void testCalculateOutputStack() {
+    }
+
+    public void testFirstIsGreaterInPrecedence() {
+    }
+}
