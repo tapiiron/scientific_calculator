44f65e2ab81c576fa65a409ead5d01ccb5464739
Tapio Piironen <tapio.piironen@helsinki.fi>
2026-01-20 16:34:28 +0200

    initial implementation of pseudo code for Shunting Yard algorithm

--- src/main/java/fi/helsinki/tapio/tools/ShuntingYardTool.java
+++ src/main/java/fi/helsinki/tapio/tools/ShuntingYardTool.java
@@ -1,4 +1,6 @@
-package fi.helsinki.tapio.tools.ShunthingYardTool.java;
+package fi.helsinki.tapio.tools;
+
+import java.util.Stack;
 
 public class ShuntingYardTool {
     /**
@@ -35,12 +37,81 @@ while there are tokens on the operator stack:
     pop the operator from the operator stack onto the output queue
      */
 
-    public void calculateOutputStack(String input) throws Exception {
+    public String calculateOutputStack(String input) throws Exception {
         if (input == null || input.length()<1) {
             throw new Exception("Invalid input");
         }
-        // Remove all spaces
+        // Remove all spaces and lowercase
         input = input.replace(" ",""); 
-        // tokens = 
+        input = input.toLowerCase();
+
+        int index = 0;
+        char[] tokens = input.toCharArray();
+
+        // Output queue
+        String outputQueue = "";
+
+        // Operator stack
+        Stack<String> operatorStack = new Stack<>();
+
+        // Main loop of input characters
+        while (index<tokens.length) {
+            char token = tokens[index];
+
+            if (Character.isDigit(token)) { // If token is number
+                outputQueue += token;
+            } else if (token == 's' || token == 'm' ) { // If token is function
+                // sin, sqrt, min and max Handle skip index here
+                String funtion = ""+token+tokens[index+1]+tokens[index+2];
+                index += 2;
+                if (funtion.equals("sqr")) {
+                    funtion = "sqrt";
+                    index++;
+                }
+                // Check that funtion is valid
+                if (!funtion.equals("sin") && !funtion.equals("sqrt") && !funtion.equals("min") && !funtion.equals("max")) {
+                    throw new Exception("Invalid function: " + funtion);
+                }
+                operatorStack.push(funtion);
+            } else if (token == '+' || token == '-' || token == '*' || token == '/') { // If token is operator
+                // TODO: logic here
+                /**
+                 * while (
+                 *             there is an operator o2 at the top of the operator stack which is not a left parenthesis,
+                 *             and (o2 has greater precedence than o1 or (o1 and o2 have the same precedence and o1 is left-associative))
+                 *         ):
+                 *             pop o2 from the operator stack into the output queue
+                 *         push o1 onto the operator stack
+                 */
+                operatorStack.push(""+token);
+            } else if (token == ',') {
+                outputQueue += token;
+            } else if (token == '(') {
+                operatorStack.push(""+token);
+            } else if (token == ')') {
+                // TODO: logic here
+                /**
+                 *  while the operator at the top of the operator stack is not a left parenthesis:
+                 *             {assert the operator stack is not empty}
+                 *             pop the operator from the operator stack into the output queue
+                 *         {assert there is a left parenthesis at the top of the operator stack}
+                 *         pop the left parenthesis from the operator stack and discard it
+                 *         if there is a function token at the top of the operator stack, then:
+                 *             pop the function from the operator stack into the output queue
+                 */
+            } else {
+                throw new Exception("Invalid token: " + token);
+            }
+            index++;
+        }
+        /* while there are tokens on the operator stack:
+        {assert the operator on top of the stack is not a (left) parenthesis}
+        pop the operator from the operator stack onto the output queue
+                */
+        while (!operatorStack.empty()) {
+            outputQueue += operatorStack.pop();
+        }
+
+        return outputQueue;
     }
 }
